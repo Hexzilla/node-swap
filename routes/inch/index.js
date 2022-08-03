de603fe7ba71fcc6ee3dbccee094430523415f7d
Eric Thomas <bluebird6922@gmail.com>
2022-08-03 10:35:17 +0300

    update token

--- routes/inch/index.js
+++ routes/inch/index.js
@@ -5,14 +5,6 @@ const isNullOrUndefined = (value) => {
   return value === null || value === undefined
 }
 
-router.get('/:tokenId', (req, res, next) => {
-  axios.get(`https://token-prices.1inch.io/v1.1/${req.params.tokenid}`)
-    .then((result) => {
-      return res.json(result.data)
-    })
-    .catch(console.error)
-})
-
 router.get('/distribution/v2/:address', (req, res, next) => {
   axios.get(`https://governance.1inch.io/v1.1/distribution/v2/${req.params.address}`)
     .then((result) => {
@@ -23,6 +15,17 @@ router.get('/distribution/v2/:address', (req, res, next) => {
 
 router.get('/distribution/:address', (req, res, next) => {
   axios.get(`https://governance.1inch.io/v1.1/distribution/${req.params.address}`)
+    .then((result) => {
+      return res.json(result.data)
+    })
+    .catch(console.error)
+})
+
+router.get('/history/:tokenId/:address', (req, res, next) => {
+  const { tokenId, address } = req.params;
+  const { not_send_call_data, limit } = req.query;
+  const url = `https://history.1inch.io/v1.0/history/${tokenId}/${address}?not_send_call_data=${not_send_call_data}&limit=${limit}`
+  axios.get(url)
     .then((result) => {
       return res.json(result.data)
     })
@@ -62,17 +65,12 @@ router.get('/:tokenId/allowancesAndBalances/:contract/:address', (req, res, next
     .catch(console.error)
 })
 
-router.get('/history/:tokenId/:address', (req, res, next) => {
-  const { tokenId, address } = req.params;
-  const { not_send_call_data, limit } = req.query;
-  const url = `https://history.1inch.io/v1.0/history/${tokenId}/${address}?not_send_call_data=${not_send_call_data}&limit=${limit}`
-  axios.get(url)
+router.get('/:tokenId', (req, res, next) => {
+  const { tokenId } = req.params;
+  axios.get(`https://token-prices.1inch.io/v1.1/${tokenId}`)
     .then((result) => {
       return res.json(result.data)
     })
-    .catch(console.error)
 })
-
-
 
 module.exports = router
